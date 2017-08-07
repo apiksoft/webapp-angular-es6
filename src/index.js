705af19a1eb202bfd180dba6596327cc671b4274
Kadzany <khaerul.adzany@gmail.com>
2017-08-07 15:20:59 +0700

    bootstrapping app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import { appname } from './config';
+
+angular.module(appname, []);
+
+angular.element(document).ready(() => {
+    angular.bootstrap(document, [appname]);
+});
+
 console.log(angular);
